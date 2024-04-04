f75e6dfb324d5e0a3b05f1a968392f04bf9b1bbf
sigitabra <sigita.zutautaite@gmail.com>
2024-04-04 18:10:08 +0300

    Create tables and relationships with hibernate

---
+++ src/main/java/org/example/entities/Department.java
@@ -0,0 +1,23 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "skyrius")
+public class Department {
+    @Id
+    @Column(name = "pavadinimas")
+    private String title;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Employee manager;
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    private List<Employee> employeesByDep = new ArrayList<>();
+
+}
